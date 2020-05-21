d17057c523f394c2abaf61511ca160931a677f04
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2020-05-21 15:41:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /* -----------------------------------------------
 /* How to use? : Check the GitHub README
 /* ----------------------------------------------- */
-
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 /*
 particlesJS.load('particles-js', 'particles.json', function() {
