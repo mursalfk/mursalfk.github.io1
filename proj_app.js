051fb1c7531f85a8093a586dd0ac750e8e7ee421
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2020-05-23 02:42:32 +0500

    Update proj_app.js

--- proj_app.js
+++ proj_app.js
@@ -8,9 +8,6 @@ xhr.onload = function () {
         ul.classList.add('list-inline')
         let li = document.createElement('li');
         li.classList.add('list-inline-item')
-        // li.classList.add('col-sm-4 my-3')
-        // li.classList.add('card border-0')
-
         li.innerHTML = (`
             <li class="col-sm-0 card" style="margin-top:25px; padding:20px; width:250px; overflow:hidden; height:300px">
                 <p><strong>Project:</strong> <a href="${data[i].html_url}" target="_blank"><h4>${data[i].name}</h4></a></p>
